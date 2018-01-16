b21938d6efcc834486e65eb6e446985e20eb30b3
Shivendra Sharma <shivendra.sharma090@yahoo.com>
2018-01-16 12:56:49 +0530

    Revert "First commit"
    This reverts commit 67adab8cffd6b686b2e45f45b31ac19596a05292.

--- Reading_csv_files.py
+++
@@ -1,7 +0,0 @@
-# Out first DictReader script. While soft warap is enabled, this python file will remain softwrapped.
-
-with open('Chicago-Divy-2016.csv', 'r') as trip_file:
-      csv_reader = csv.DictReader(trip_file)
-
-      for line in csv_reader:
-          print(line)
